adcb7be7cf66c02c775cf1a386a6b2e673f6e336
omkarg01 <omkargujja01@gmail.com>
2022-09-01 00:00:37 +0530

    15 questions completed

--- variable.js
+++ variable.js
@@ -11,8 +11,96 @@
 // console.log(typeof no);
 
 // 2 ------------------------------------------------------------
-const first_name = "omkar";
-const last_name = "gujja";
-const age = 22;
-const is_married = false;
-console.log(`${first_name} ${last_name} ${age} ${is_married}`);
+// const first_name = "omkar";
+// const last_name = "gujja";
+// const age = 22;
+// const is_married = false;
+// console.log(`${first_name} ${last_name} ${age} ${is_married}`);
+
+// 3 ------------------------------------------------------------
+// const first_line = "Hello World";
+// console.log(first_line.toUpperCase());
+
+// 4 ------------------------------------------------------------
+// const lang = "This Script is for automation it is written in JavaScript";
+// console.log(lang.includes("Script"));
+
+// 5 ------------------------------------------------------------
+// const intro = "My name is Omkar Gujja";
+// console.log(intro.split(" "));
+
+// 6 ------------------------------------------------------------
+// const mnc = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+// console.log(mnc.split(', '));
+
+// 7 ------------------------------------------------------------
+// console.log(lang.lastIndexOf("Script"));
+
+// 8 ------------------------------------------------------------
+// const sentence = '  You cannot end a sentence with because because because is a conjunction   ';
+// console.log(sentence.indexOf('because'));
+
+// 9 ------------------------------------------------------------
+// console.log(sentence.trim());
+
+// 10 ------------------------------------------------------------
+// const a = "Hello";
+// const b = "World";
+// const c = "Hello";
+
+// console.log(a === c);
+// console.log(a === b);
+
+// 11 ------------------------------------------------------------
+// console.log(4 > 3);
+// console.log(4 >= 3);
+// console.log(4 < 3);
+// console.log(4 <= 3);
+// console.log(4 == 4);
+// console.log(4 === 4);
+// console.log(4 != 4);
+// console.log(4 !== 4);
+// console.log(4 != '4');
+// console.log(4 == '4');
+// console.log(4 === '4');
+
+// const str1 = "python";
+// const str2 = "jargon";
+// if (str1.length == str2.length) {
+//     console.log("Lengths are equal");
+// } else {
+//     console.log("lengths are not equal");
+// }
+
+// 12 ------------------------------------------------------------
+// const date = new Date();
+// console.log(date);
+// const year = date.getFullYear();
+// console.log(year);
+// const month = date.getMonth();
+// console.log(month);
+// const day = date.getDay();
+// console.log(day);
+// const hours = date.getHours();
+// console.log(hours);
+// const minutes = date.getMinutes();
+// console.log(minutes);
+// var now = new Date();
+// var bDay = new Date(1979, 1, 1);
+// var elapsedT = now - bDay; // in ms
+// console.log(elapsedT / 1000);
+
+// 13 ------------------------------------------------------------
+// let base = prompt("Enter a base");
+// let height = prompt("Enter a height");
+// let area = (base * height) / 2;
+// console.log(area);
+
+// 14 ------------------------------------------------------------
+// const x1 = 2;
+// const y1 = 2;
+// const x2 = 6;
+// const y2 = 10;
+// const m = (y2 - y1) / (x2 - x1);
+// console.log("Slope", m);
+
